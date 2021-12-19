7acc45f3fba68cba0761839c8a1538d556f213d2
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-19 17:30:33 -0500

    Contrado de oraculo creado

---
+++ contracts/Oracle.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Oracle {
+    // Direccion del owner
+    address owner;
+
+    // Numero de asteroides
+    uint public numberAsteroids;
+
+    // Evento que reciba datos del oraculo
+    event __callbackNewData();
+
+    // Constructor
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    // Restriccion de la ejecucion de las funciones
+    modifier onlyOwner() {
+        require(msg.sender == owner, 'Only owner');
+        _;
+    }
+
+    // Recibe datos del oraculo
+    function update() public onlyOwner() {
+        emit __callbackNewData();
+    }
+
+    // Funcion para configuracion manual del número de asteroides
+    function setNumberAsterioids(uint _num) public onlyOwner {
+        numberAsteroids = _num;
+    }
+
+
+}
